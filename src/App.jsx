99f8d7cb5446785f2ea276f4182b10956c591fe6
İbrahim Basmacı <ibrbsmc@gmail.com>
2026-07-31 21:59:39 +0300

    chore: configure Tailwind CSS and shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold">Kutula</h1>
+        <Button>Yeni Kutu Ekle</Button>
+      </main>
+    </>
+  );
 }
 
 export default App;
